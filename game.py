a7ed2c0c191de4a13e623b7ca1c5e8a1f7b05ae0
Stephen Jones <immobilesteelrims@gmail.com>
2025-11-03 18:37:48 -0800

    add multiple rooms

--- game.py
+++ game.py
@@ -19,62 +19,99 @@
 #         print("AND...")
 #     print(f"I love {thing}!")
 
+
+class Room:
+    def __init__(self):
+        self.inventory = {}
+        self.targets = {}
+        self.data = {}
+        self.paths = {}
+
+    def look(self):
+        print("You look around.")
+        for target in self.targets:
+            if target == "hole":
+                print("There's a hole in the wall!")
+            elif target == "tree":
+                print("There's a beautiful tree you planted!")
+            elif target == "dirt":
+                print("There's dirt on the ground.")
+        for item in self.inventory:
+            print(f"You see a {item}.")
+
+    def use(self, item, target):
+        if target in self.targets:
+            print(f"You use the {item} on the {target}.")
+            if item == "block" and target == "hole":
+                print("You fixed the hole in the wall!")
+                print("You win the game!")
+                inventory.discard("block")
+                self.targets.discard("hole")
+                hole_in_the_wall = False
+                return
+            elif item == "tree-seed" and target == "dirt":
+                print("You planted a tree and it grew!")
+                inventory.discard("tree-seed")
+                self.targets.discard("dirt")
+                self.targets.add("tree")
+            elif "block" in item and target == "lava":
+                if "blocks" not in self.data:
+                    self.data["blocks"] = set()
+                print(f"You threw the {item} in the lava and it floats!")
+                self.data["blocks"].add(item)
+            else:
+                print("Nothing happened.")
+        else:
+            print(f"There's no {target}.")
+
+    def move(self, direction):
+        if direction in self.paths:
+            return self.paths[direction]
+        else:
+            print("You can't go that way.")
+            return self
+
+
+OVERWORLD = Room()
+
+LAVA_PIT = Room()
+LAVA_PIT.inventory = {"peanut", "apple", "block-one", "block-two", "block-three"}
+LAVA_PIT.targets = {"lava"}
+
+END = Room()
+END.inventory = {"block", "scooter", "broken-torch"}
+END.targets = {"hole", "dirt"}
+
+LAVA_PIT.paths = {"north": OVERWORLD, "south": END}
+
 inventory = {"tree-seed", "shovel"}
-room_inventory = {"block", "scooter", "broken-torch"}
-room_targets = {"hole", "dirt"}
 
 while True:
     command = input("What do you want to do? ")
     match command.split():
         case ["pickup", item]:
-            if item in room_inventory:
+            if item in END.inventory:
                 print(f"You picked up the {item}.")
                 inventory.add(item)
-                room_inventory.discard(item)
+                END.inventory.discard(item)
             else:
                 print(f"There's no {item} in the room.")
         case ["drop", item]:
             if item in inventory:
                 print(f"You dropped the {item}.")
                 inventory.discard(item)
-                room_inventory.add(item)
+                END.inventory.add(item)
             else:
                 print(f"There's no {item} in your inventory.")
         case ["look"]:
-            print("You look around.")
-            for target in room_targets:
-                if target == "hole":
-                    print("There's a hole in the wall!")
-                elif target == "tree":
-                    print("There's a beautiful tree you planted!")
-                elif target == "dirt":
-                    print("There's dirt on the ground.")
-            for item in room_inventory:
-                print(f"You see a {item}.")
+            END.look()
         case ["inventory"]:
             print("You check your inventory.")
             for item in inventory:
                 print(f"You see a {item}.")
         case ["use", item, "on", target]:
             if item in inventory:
-                if target in room_targets:
-                    print(f"You use the {item} on the {target}.")
-                    if item == "block" and target == "hole":
-                        print("You fixed the hole in the wall!")
-                        print("You win the game!")
-                        inventory.discard("block")
-                        room_targets.discard("hole")
-                        hole_in_the_wall = False
-                        break
-                    elif item == "tree-seed" and target == "dirt":
-                        print("You planted a tree and it grew!")
-                        inventory.discard("tree-seed")
-                        room_targets.discard("dirt")
-                        room_targets.add("tree")
-                    else:
-                        print("Nothing happened.")
-                else:
-                    print(f"There's no {target}.")
+                END.use(item, target)
             else:
                 print(f"You don't have {item} in your inventory.")
         case ["quit"]:
